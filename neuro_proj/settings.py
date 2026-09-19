1bba08791b98cec0d4496fb6f373af89b78ad112
feralsyntax <fullstack.benie@gmail.com>
2026-09-19 12:46:13 +0300

    change (settings): remove default parser classes in drf configs

--- neuro_proj/settings.py
+++ neuro_proj/settings.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=False, cast=bool)
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 MODE = config("MODE", default="dev", cast=str)
 SECRET_KEY = config("SECRET_KEY")
 
@@ -56,9 +56,6 @@ ROOT_URLCONF = "neuro_proj.urls"
 # Rest Framework Configs
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PARSER_CLASSES": [
-        "rest_framework.parsers.JSONParser",
-    ],
 }
 
 TEMPLATES = [
